9518a8897c02c7678962dd155ebbaa81db33488a
Razzer101 <Rasmus@anders-net.dk>
2026-05-18 12:52:11 +0200

    refactor: Removed name from routes in main.js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/Home.vue'
 
 const routes = [
-        {path: "/", name: "Home", component: Home},
-        {path: "/about", name: "About", component: () => import("./pages/About.vue")},
-        {path: "/contact", name: "Contact", component: () => import("./pages/Contact.vue")},
+        {path: "/", component: Home},
+        {path: "/about", component: () => import("./pages/About.vue")},
+        {path: "/contact", component: () => import("./pages/Contact.vue")},
         {path: "/product/:id", component: () => import("./pages/Product.vue")}
     ]
 
